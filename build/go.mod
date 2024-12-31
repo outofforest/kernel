579081657065b681776893aad29f2e660bd38db4
Wojciech Malota-Wojcik <wojtek@exw.co>
2024-12-31 20:31:24 +0100

    Create efi image

--- build/go.mod
+++ build/go.mod
@@ -10,16 +10,25 @@ require (
 )
 
 require (
+	github.com/diskfs/go-diskfs v1.5.0 // indirect
+	github.com/djherbis/times v1.6.0 // indirect
+	github.com/elliotwutingfeng/asciiset v0.0.0-20240214025120-24af97c84155 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
 	github.com/outofforest/ioc/v2 v2.5.2 // indirect
 	github.com/outofforest/libexec v0.3.9 // indirect
 	github.com/outofforest/logger v0.5.5 // indirect
 	github.com/outofforest/parallel v0.2.3 // indirect
 	github.com/outofforest/run v0.8.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.22 // indirect
+	github.com/pkg/xattr v0.4.10 // indirect
 	github.com/samber/lo v1.47.0 // indirect
+	github.com/sirupsen/logrus v1.9.4-0.20230606125235-dd1b4c2e81af // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/ulikunitz/xz v0.5.12 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 )
--- build/loader.go
+++ build/loader.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 
 	"github.com/cavaliergopher/cpio"
+	"github.com/diskfs/go-diskfs/backend/file"
+	"github.com/diskfs/go-diskfs/filesystem/fat32"
 	"github.com/pkg/errors"
 
 	"github.com/outofforest/build/v2/pkg/tools"
@@ -28,10 +30,47 @@ const (
 func buildLoader(ctx context.Context, deps types.DepsFunc) error {
 	deps(prepareEmbeds, zig.EnsureZig)
 
-	return zig.Build(ctx, deps, zig.BuildConfig{
+	if err := zig.Build(ctx, deps, zig.BuildConfig{
 		PackagePath: "loader",
 		OutputPath:  "bin",
-	})
+	}); err != nil {
+		return err
+	}
+
+	inF, err := os.Open("bin/bootx64.efi")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer inF.Close()
+
+	if err := os.Remove("bin/efi.img"); err != nil && !os.IsNotExist(err) {
+		return errors.WithStack(err)
+	}
+
+	const size = 1024 * 1024 * 1024
+	b, err := file.CreateFromPath("bin/efi.img", size)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer b.Close()
+
+	fs, err := fat32.Create(b, size, 0, 0, "efi")
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if err := fs.Mkdir("/EFI/BOOT"); err != nil {
+		return errors.WithStack(err)
+	}
+
+	outF, err := fs.OpenFile("/EFI/BOOT/bootx64.efi", os.O_RDWR|os.O_TRUNC|os.O_CREATE)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	defer outF.Close()
+
+	_, err = io.Copy(outF, inF)
+	return errors.WithStack(err)
 }
 
 func prepareEmbeds(ctx context.Context, deps types.DepsFunc) error {
